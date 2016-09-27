c9f5e929b99ebdc7b518848051af175fdc90d8c1
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-09-27 21:00:14 +0300

    add state to Article

--- src/Article.js
+++ src/Article.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+export default class Article extends Component {
+
+    state = {
+        isOpen: false
+    }
+
+    render() {
+        const { article } = this.props
+        const { isOpen } = this.state
+
+        const body = isOpen ? <section>{article.text}</section> : null
+
+        return (
+            <div>
+                <h3 onClick = {this.toggleOpen}>{article.title}</h3>
+                {body}
+            </div>
+        )
+    }
+
+    toggleOpen = ev => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
+
+
+
+/*
 export default (props) => {
     const { article } = props
 
@@ -9,4 +39,4 @@ export default (props) => {
             <section>{article.text}</section>
         </div>
     )
-}
+}*/
