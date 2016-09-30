0f953323ec49b84dede6ff92ff30c9b903a40c57
Igor Kucheynyk <ikucheynyk@apple.com>
2016-09-30 09:05:24 -0700

    Added toggle in comment list

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Comment from './Comment'
 
-export default (props) => {
-	const { comments } = props
+export default class CommentList extends Component {
 
-	const commentComponents = comments.map(comment => <li key={comment.id}><Comment comment={comment} /></li>)
+	state = {
+		isOpen: false
+	}
 
-	return (
-		<ul>
-		{commentComponents}
-		</ul>
-	)
+	render() {
+		const { comments } = this.props
+		const { isOpen } = this.state
+		let body = null;
+
+		if (isOpen) {
+			body = (
+				<ul>
+					{comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)}
+				</ul>
+			)
+		}
+
+		return (
+			<div>
+				<a href = "javascript:void(0)" onClick = {this.toggleOpen} > Comments < / a >
+				{body}
+			</div>
+		)
+	}
+
+	toggleOpen = ev => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	}
+
 }
