8515d48db997771ee3bb94d0d8a808f6acea4f7a
Roman <kaboom.ua@gmail.com>
2016-10-03 21:59:52 +0300

    Update accordion.js

--- src/decorators/accordion.js
+++ src/decorators/accordion.js
@@ -5,6 +5,7 @@ import React from "react"
 export default function accordion(Component) {
   return class WrappedComponent extends React.Component {
     state = {
+      //не привязывайся к названию сущности, в декораторах должен быть общий функционал. Используй, например, openItemId 
       openArticleId: null
     }
 
