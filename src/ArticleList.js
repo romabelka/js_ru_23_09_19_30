c6b02acc39efe21a1dc6b51316def9f2a93cf87c
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-10-04 18:37:27 +0300

    add accordion decorator

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react'
 import Article from './Article'
 import Chart from './Chart'
+import accordion from './decorators/accordion'
 
 class ArticleList extends Component {
     static propTypes = {
 
     };
 
-    state = {
-        openArticleId: null
-    }
-
     render() {
-        const { articles } = this.props
-        const { openArticleId } = this.state
+        const { articles, toggleItem, isItemOpen } = this.props
 
         const articleComponents = articles.map(article => (
             <li key={article.id} >
-                <Article article = {article} isOpen = {article.id == openArticleId} openArticle = {this.toggleOpenArticle(article.id)} />
+                <Article article = {article} isOpen = {isItemOpen(article.id)} openArticle = {toggleItem(article.id)} />
             </li>))
 
         return (
@@ -29,12 +25,6 @@ class ArticleList extends Component {
             </div>
         )
     }
-
-    toggleOpenArticle = id => ev => {
-        this.setState({
-            openArticleId: id
-        })
-    }
 }
 
-export default ArticleList
+export default accordion(ArticleList)
