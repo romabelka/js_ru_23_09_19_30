b847eb055f5f377cfeaaabaa81497682d8e96764
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-10-07 18:38:31 +0300

    add day-picker

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,33 +1,17 @@
 import React, { Component, PropTypes } from 'react'
-import Select from 'react-select'
-import 'react-select/dist/react-select.css'
+import DatePicker from './DatePicker'
+import SelectFilter from './SelectFilter'
 
 class Filter extends Component {
     static propTypes = {
         articles: PropTypes.array.isRequired
     };
 
-    state = {
-        selected: null
-    }
-
-    handleChange = selected => this.setState({ selected })
-
     render() {
-        const { articles } = this.props
-        const { selected } = this.state
-        const options = articles.map(article => ({
-            label: article.title,
-            value: article.id
-        }))
         return (
             <div>
-                <Select
-                    options = {options}
-                    value = {selected}
-                    multi = {true}
-                    onChange = {this.handleChange}
-                />
+                <DatePicker />
+                <SelectFilter articles = {this.props.articles} />
             </div>
         )
     }
