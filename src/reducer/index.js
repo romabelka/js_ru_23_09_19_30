9cbc0b542605260772fd9e03c60a98a5df4c79ff
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-10-11 18:34:22 +0300

    add filters

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
 import count from './counter'
 import articles from './articles'
+import filters from './filters'
 
 export default combineReducers({
-    articles, count
+    articles, count, filters
 })
