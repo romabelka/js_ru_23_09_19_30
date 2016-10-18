ad352bcf734cd171a8bcbce043fe4a1cba3b6beb
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-10-18 15:41:03 +0300

    move to immutable

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -32,7 +32,7 @@ export default connect(state => {
     const selected = filters.get('selected')
     const { from, to } = filters.get('dateRange')
 
-    const articleArray = Object.keys(articles).map(id => articles[id])
+    const articleArray = articles.valueSeq().toArray()
     const filteredArticles = articleArray.filter(article => {
         const published = Date.parse(article.date)
         return (!selected.length || selected.includes(article.id)) &&
